6d5fc95f2eea74f3510e4724f4e1eb11e727131d
Charly Laurent <charly.laurent@eureden.com>
2021-07-02 10:43:35 +0200

    implement Ligth

--- christmas_ligths.test.ts
+++ christmas_ligths.test.ts
@@ -1,9 +1,50 @@
-function sum(a: number, b: number) {
-    return a + b;
+class Light {
+    public isOn: boolean;
+
+    constructor() {
+        this.isOn = false;
+    }
+
+    turnOn(): void {
+        this.isOn = true;
+    }
+
+    turnOff(): void {
+        this.isOn = false;
+    }
+
+    toggle(): void {
+        this.isOn = !this.isOn;
+    }
 }
 
-describe('christmas ligths', () => {
-    test('sum', () => {
-        expect(sum(1, 2)).toBe(3);
+describe("Ligth", () => {
+    test("init", () => {
+        const ligth = new Light();
+        expect(ligth.isOn).toBe(false);
+    })
+
+    test("turnOn should turn on", () => {
+        const ligth = new Light();
+        ligth.turnOn();
+        expect(ligth.isOn).toBe(true);
+    })
+
+    test("turnOff should turn off", () => {
+        const ligth = new Light();
+
+        ligth.turnOn();
+        ligth.turnOff();
+        expect(ligth.isOn).toBe(false);
+    })
+
+    test("toggle should turn on then off", () => {
+        const ligth = new Light();
+
+        ligth.toggle();
+        expect(ligth.isOn).toBe(true);
+
+        ligth.toggle();
+        expect(ligth.isOn).toBe(false);
     })
 })
