24cacfd509e7082b04ce54af34fb5318294f2856
Charly Laurent <charly.laurent@eureden.com>
2021-07-05 09:46:15 +0200

    test kata part 1

--- grid.test.ts
+++ grid.test.ts
@@ -68,4 +68,20 @@ describe("Grid", () => {
             expect(grid.isLigthOn(point)).toBe(false)
         }
     })
+
+    test("kata part 1", () => {
+        const grid = new Grid(1000, 1000);
+
+        grid.turnOnThrough({ x: 887, y: 9 }, { x: 959, y: 629 });
+        grid.turnOnThrough({ x: 454, y: 398 }, { x: 844, y: 448 });
+        grid.turnOffThrough({ x: 539, y: 243 }, { x: 559, y: 965 });
+        grid.turnOffThrough({ x: 370, y: 819 }, { x: 676, y: 868 });
+        grid.turnOffThrough({ x: 145, y: 40 }, { x: 370, y: 997 });
+        grid.turnOffThrough({ x: 301, y: 3 }, { x: 808, y: 453 });
+        grid.turnOnThrough({ x: 351, y: 678 }, { x: 951, y: 908 });
+        grid.toggleThrough({ x: 720, y: 196 }, { x: 897, y: 994 });
+        grid.toggleThrough({ x: 831, y: 394 }, { x: 904, y: 860 });
+
+        expect(grid.countLightsOn()).toBe(230022);
+    })
 })
