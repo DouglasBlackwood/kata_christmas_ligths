1fe76b016259bc3eea9a7e4505a5af6202cc6a1d
Charly Laurent <charly.laurent@eureden.com>
2021-07-05 10:28:58 +0200

    implement BrightLight

--- ligth.test.ts
+++ ligth.test.ts
@@ -1,4 +1,4 @@
-import { Light } from './ligth';
+import { Light, BrightLight } from './ligth';
 
 describe("Ligth", () => {
     test("init", () => {
@@ -28,5 +28,43 @@ describe("Ligth", () => {
 
         ligth.toggle();
         expect(ligth.isOn).toBe(false);
+    })
+})
+
+describe("BrightLigth", () => {
+    test("init", () => {
+        const ligth = new BrightLight();
+        expect(ligth.brightness).toBe(0);
+        expect(ligth.isOn).toBe(false);
+    })
+
+    test("turnOn should increment brightness", () => {
+        const ligth = new BrightLight();
+        ligth.turnOn();
+        expect(ligth.brightness).toBe(1);
+        expect(ligth.isOn).toBe(true);
+
+        ligth.turnOn();
+        expect(ligth.brightness).toBe(2);
+        expect(ligth.isOn).toBe(true);
+    })
+
+    test("turnOff should decrement brightness with a minimum of 0", () => {
+        const ligth = new BrightLight();
+
+        ligth.turnOn();
+        ligth.turnOff();
+        expect(ligth.brightness).toBe(0);
+        expect(ligth.isOn).toBe(false);
+
+        ligth.turnOff();
+        expect(ligth.brightness).toBe(0);
+    })
+
+    test("toggle should increase brightness by 2", () => {
+        const ligth = new BrightLight();
+
+        ligth.toggle();
+        expect(ligth.brightness).toBe(2);
     })
 })
--- ligth.ts
+++ ligth.ts
@@ -1,19 +1,58 @@
-export class Light {
-    public isOn: boolean;
+class Light {
+    private _isOn: boolean;
 
     constructor() {
-        this.isOn = false;
+        this._isOn = false;
+    }
+
+    public get isOn(): boolean {
+        return this._isOn;
     }
 
     turnOn(): void {
-        this.isOn = true;
+        this._isOn = true;
     }
 
     turnOff(): void {
-        this.isOn = false;
+        this._isOn = false;
     }
 
     toggle(): void {
-        this.isOn = !this.isOn;
+        this._isOn = !this._isOn;
     }
 }
+
+class BrightLight extends Light {
+    private _brightness: number;
+
+    constructor() {
+        super();
+
+        this._brightness = 0;
+    }
+
+    public get isOn(): boolean {
+        return this._brightness > 0;
+    }
+
+    public get brightness(): number {
+        return this._brightness;
+    }
+
+    turnOn(): void {
+        this._brightness++;
+    }
+
+    turnOff(): void {
+        this._brightness--;
+        if (this._brightness < 0) {
+            this._brightness = 0;
+        }
+    }
+
+    toggle(): void {
+        this._brightness += 2;
+    }
+}
+
+export { Light, BrightLight };
